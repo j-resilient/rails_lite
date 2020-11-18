e8bd419334068098372047d114456abc46340a9a
J Resilient <j.resilient.1@gmail.com>
2020-11-18 18:37:35 -0500

    Add code to Session to handle adding/updating/accessing session cookie

--- lib/session.rb
+++ lib/session.rb
@@ -4,16 +4,20 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @session = (req.cookies['_rails_lite_app'] ? JSON.parse(req.cookies["_rails_lite_app"]) : {})
   end
 
   def [](key)
+    @session[key]
   end
 
   def []=(key, val)
+    @session[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie("_rails_lite_app", {path: "/", value: @session.to_json})
   end
 end
