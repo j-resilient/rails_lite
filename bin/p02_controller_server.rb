5434ec3b3397eb97e53fd54d0dd4d55d6d1e10e9
J Resilient <j.resilient.1@gmail.com>
2020-11-20 18:53:15 -0500

    Add comments to p02_controller_server.rb to demonstrate I understand the skeleton code

--- bin/p02_controller_server.rb
+++ bin/p02_controller_server.rb
@@ -1,8 +1,11 @@
 require 'rack'
 require_relative '../lib/controller_base'
 
+# creates the MyController class which inherits from ControllerBase
 class MyController < ControllerBase
   def go
+    # depending on the requests path call either #render_content or #redirect_to
+    # both of which are defined in ControllerBase
     if req.path == "/cats"
       render_content("hello cats!", "text/html")
     else
@@ -11,13 +14,25 @@ class MyController < ControllerBase
   end
 end
 
+# the rack app
 app = Proc.new do |env|
+  # create a request object with all of the data from env
+  # which is all of the data from the HTTP request
   req = Rack::Request.new(env)
+  # create a blank response object
   res = Rack::Response.new
+  # instantiate a copy of the MyController controller
+  # passing req and res to #initialize, which is defined in ControllerBase
+  # then call #go
   MyController.new(req, res).go
+  # now that the response has been updated, use #finish to convert it
+  # to the rack standard array format
+  # then return it
   res.finish
 end
 
+# start the server listening on port 3000
+# and mount the rack app on it
 Rack::Server.start(
   app: app,
   Port: 3000
