9a47b234ee69d08fa31f57d70f153b31ca7777de
J Resilient <j.resilient.1@gmail.com>
2020-12-01 19:49:39 -0500

    Add basics of getting and setting the flash to Flash

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,19 @@
 require 'json'
 
 class Flash
+    def initialize(req)
+        @flash = (req.cookies['_rails_lite_app_flash'] ? JSON.parse(req.cookies['_rails_lite_app_flash']) : {})
+    end
+
+    def [](key)
+        @flash[key.to_sym]
+    end
+
+    def []=(key, value)
+        @flash[key.to_sym] = value
+    end
+
+    def store_flash(res)
+        res.set_cookie("_rails_lite_app_flash", {path: "/", value: @flash.to_json})
+    end
 end
