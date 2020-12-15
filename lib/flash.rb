5a4d7ed5209912690bcbcfe3b053d13cd40e35f7
J Resilient <j.resilient.1@gmail.com>
2020-12-15 16:16:11 -0500

    Change Flash so that the contents of flash only live for one request/response cycle.

--- lib/flash.rb
+++ lib/flash.rb
@@ -2,10 +2,8 @@ require 'json'
 
 class Flash
     def initialize(req)
-        # cookie = (req.cookies['_rails_lite_app_flash'] ? JSON.parse(req.cookies['_rails_lite_app_flash']) : {life_count: 1})
-        # cookie = {life_count: 1} if cookie[:life_count] == 0
-
-        @flash = (req.cookies['_rails_lite_app_flash'] ? JSON.parse(req.cookies['_rails_lite_app_flash']) : {})
+        @cookie = (req.cookies['_rails_lite_app_flash'] ? JSON.parse(req.cookies['_rails_lite_app_flash']) : {}) 
+        @flash = (req.cookies['_rails_lite_app_flash'] ? JSON.parse(req.cookies['_rails_lite_app_flash']) : {}) 
     end
 
     def [](key)
@@ -19,7 +17,7 @@ class Flash
     end
 
     def store_flash(res)
-        # @flash[:life_count] = (@flash[:life_count] - 1)
+        @flash.reject! { |key, val| @cookie[key] == val }
         res.set_cookie("_rails_lite_app_flash", {path: "/", value: @flash.to_json})
     end
 end
