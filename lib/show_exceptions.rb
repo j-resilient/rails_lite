eb4aec565f3bca852a2208e79596f765483021d7
J Resilient <j.resilient.1@gmail.com>
2021-01-04 18:35:37 -0500

    Add basic middleware code and begin-rescue block to show_exceptions.rb

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -2,14 +2,24 @@ require 'erb'
 
 class ShowExceptions
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      @app.call(env)
+    rescue Exception => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    print e
+    # render the stack trace
+    # preview the code where the exception was raised
+    # display the exception method
   end
 
 end
