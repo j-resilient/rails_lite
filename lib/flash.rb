1c55fef94f7a03d48c18e9062f125937dab5ca5c
J Resilient <j.resilient.1@gmail.com>
2021-01-04 18:11:57 -0500

    Add comments to flash.rb

--- lib/flash.rb
+++ lib/flash.rb
@@ -4,13 +4,19 @@ class Flash
     attr_accessor :now
 
     def initialize(req)
+        # a copy of the cookie
         @cookie = (req.cookies['_rails_lite_app_flash'] ? JSON.parse(req.cookies['_rails_lite_app_flash']) : {}) 
+        # the actual flash object
         @flash = (req.cookies['_rails_lite_app_flash'] ? JSON.parse(req.cookies['_rails_lite_app_flash']) : {}) 
+        # the flash.now object
         @now = {}
     end
 
     def [](key)
         key = key.to_s
+        # if key exists on flash object, return that
+        # else if flash.now object contains key, return that
+        # else return nil
         @flash[key] || @now[key]
     end
 
@@ -20,6 +26,7 @@ class Flash
     end
 
     def store_flash(res)
+        # get rid of any values from the last cycle before returning response
         @flash.reject! { |key, val| @cookie[key] == val }
         res.set_cookie("_rails_lite_app_flash", {path: "/", value: @flash.to_json})
     end
