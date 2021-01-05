376fadc3f225de5bf1fd927ef7004ddbc3d1686a
J Resilient <j.resilient.1@gmail.com>
2021-01-04 20:30:16 -0500

    Add code to render template for exceptions in show_exceptions.rb

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -11,18 +11,25 @@ class ShowExceptions
     begin
       @app.call(env)
     rescue Exception => e
+      # render a template with the data from the exception
       render_exception(e)
-      ['500', {'Content-type' => 'text/html'}, [e.message]]
     end
   end
 
   private
 
   def render_exception(e)
-    print e
-    # render the stack trace
-    # preview the code where the exception was raised
-    # display the exception method
+    context_info = e.backtrace.first.split(":")
+    context_file = IO.readlines(context_info.first, chomp: true)
+    index = context_info[1].to_i
+    context = context_file[(index - 5),(index + 5)]
+
+    contents = File.read("lib/templates/rescue.html.erb")
+    view = ERB.new(contents).result(binding)
+    ['500', {'Content-type' => 'text/html'}, [view]]
+
+    # this rack response is what the tests are expecting
+      # ['500', {'Content-type' => 'text/html'}, [e.message]]
   end
 
 end
