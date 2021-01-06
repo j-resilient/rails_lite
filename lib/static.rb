6ab82c0147ed2aeca07e74f013d4bc3e9371c4c8
J Resilient <j.resilient.1@gmail.com>
2021-01-05 19:04:43 -0500

    Create static rack middleware to find and returned any requested static media from the /lib/public/ directory

--- lib/static.rb
+++ lib/static.rb
@@ -1,3 +1,4 @@
+require 'mimemagic'
 class Static
   attr_reader :app
   def initialize(app)
@@ -5,6 +6,30 @@ class Static
   end
 
   def call(env)
-    @app.call(env)
+    req = Rack::Request.new(env)
+
+    if req.path.index("/public/") == 0
+      return_static_media(req)
+    else
+      @app.call(env)
+    end
+  end
+
+  def return_static_media(req)
+    res = Rack::Response.new
+    file_name = "lib#{req.path}"
+
+    # if file exists, open it and write it to response
+    # else 404 error
+    if File.exist?(file_name)
+      res.content_type = MimeMagic.by_magic(file_name)
+      res.write(File.read(file_name))
+    else
+      res.status = 404
+      res.content_type = "text/html"
+      res.write("File not found.")
+    end
+
+    res.finish
   end
 end
